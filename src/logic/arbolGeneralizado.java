e9ceba9a7adecab58229b23555c01ef15f845443
maico <maicold0930@gmail.com>
2025-02-26 22:33:35 -0500

    Actualizacion Maicol

--- src/logic/arbolGeneralizado.java
+++ src/logic/arbolGeneralizado.java
@@ -23,20 +23,26 @@ public class arbolGeneralizado {
         imprimir(raiz, "");
     }
 
+
+    //Caso#1:   Crear arbol
     public Nodo construir() {
         Nodo raiz = null;
         Nodo siguiente = null;
 
+
         while (index < cadena.length()) {
             char c = cadena.charAt(index);
+
 
             index++;
             con++;
             if (Character.isLetter(c)) {
                 Nodo nuevo = new Nodo(c);
 
+
                 if (raiz == null) {
                     raiz = nuevo;
+
 
                 } else {
                     siguiente.setLiga(nuevo);
@@ -44,16 +50,16 @@ public class arbolGeneralizado {
                 siguiente = nuevo;
             }
             else if (c == '{') {
-                    if (siguiente != null) {
-                        if (con == 2  ) {
-                           siguiente.setSubArbol(construir());
-                        }
-                        else {
-                            Nodo p = new Nodo(siguiente.getDato());
-                            siguiente.setSubArbol(p);
-                            p.setLiga(construir());
-                        }
+                if (siguiente != null) {
+                    if (con == 2  ) {
+                        siguiente.setSubArbol(construir());
                     }
+                    else {
+                        Nodo p = new Nodo(siguiente.getDato());
+                        siguiente.setSubArbol(p);
+                        p.setLiga(construir());
+                    }
+                }
             }
             else if (c == '}') {
                 return raiz;
@@ -62,16 +68,8 @@ public class arbolGeneralizado {
         return raiz;
     }
 
-    public void imprimir(Nodo nodo, String prefijo) {
-        if (nodo == null) return;
-        System.out.println(prefijo + nodo.getDato());
 
-        if (nodo.getSubArbol() != null) {
-            imprimir(nodo.getSubArbol(), prefijo + "  ");
-        }
-        imprimir(nodo.getLiga(), prefijo);
-    }
-
+    //Caso#2: Buscar Dato
     public Nodo buscarNodo(Nodo nodo, char valor) {
         Nodo p = nodo;
         while (p != null) {
@@ -88,9 +86,100 @@ public class arbolGeneralizado {
         }
         return null;
     }
-    public Nodo insertarLetra(char letra) {
 
+
+    //Caso#3:   Insertar Dato como hijo
+    public Nodo insertarLetra(char Hijo, char padre) {
+        Nodo NPapa = buscarNodo(raiz, padre);
+        if (NPapa == null) {
+            System.out.println("El padre ingresado no existe.");
+            return null;
+        }
+
+
+        Nodo NHijo = new Nodo(Hijo);
+        //Caso NPapa tenia hijos
+        if (NPapa.getSubArbol() == null) {
+            Nodo NPapa2 = new Nodo(NPapa.getDato());
+            NPapa.setSubArbol(NPapa2);
+            NPapa2.setLiga(NHijo);
+        }
+        //Caso NPapa no tenia hijos
+        else {
+            Nodo NTemporal = NPapa.getSubArbol();
+            while (NTemporal.getLiga() != null) {
+                NTemporal = NTemporal.getLiga();
+            }
+            NTemporal.setLiga(NHijo);
+        }
+        return NHijo;
     }
 
 
+    //Caso#5: Mostrar Raices
+    public void mostrarRaices(Nodo nodo, int C) {
+        if (nodo == null) return;
+        if(nodo.getSubArbol() != null){
+            System.out.println("Raiz Numero: "+C+", es: "+nodo.getDato());
+            C++;
+        }
+        mostrarRaices(nodo.getSubArbol(), C);
+        mostrarRaices(nodo.getLiga(), C);
+    }
+
+
+    //Caso#6: Mostrar Hijos
+    public void mostrarHijos(Nodo nodo) {
+        if (nodo == null) return;
+        if (nodo.getSubArbol() == null) {
+            System.out.println("El Nodo Hijo es: " + nodo.getDato());
+        }
+        if (nodo.getSubArbol() != null) {
+            mostrarHijos(nodo.getSubArbol().getLiga());
+        }
+        mostrarHijos(nodo.getLiga());
+    }
+
+
+    //Caso#7: Altura de un Dato
+    //Caso#7: Altura de un Dato
+    public int encontrarAltura(Nodo nodo, char valor) {
+        Nodo encontrado = buscarNodo(nodo, valor);
+        if (encontrado == null) {
+            System.out.println("El nodo no fue encontrado.");
+            return -1;
+        }
+        return obtenerAltura(encontrado) - 1;
+    }
+
+    public int obtenerAltura(Nodo nodo) {
+        if (nodo == null) return 0;
+        int alturaHijo = obtenerAltura(nodo.getSubArbol());
+        return 1 + alturaHijo;
+    }
+
+
+    //Caso#8: Nivel De un Dato
+    public void nivel(Nodo nodo, char Dato, int Nivel) {
+        if (nodo == null) return;
+        if(nodo.getDato() == Dato){
+            System.out.println("El nivel del nodo es: " +Nivel);
+            if (nodo.getSubArbol() != null) return;
+        }
+        nivel(nodo.getSubArbol(), Dato, Nivel+1);
+        nivel(nodo.getLiga(), Dato, Nivel);
+    }
+
+
+    //Caso#9:   Imprimir Arbol
+    public void imprimir(Nodo nodo, String prefijo) {
+        if (nodo == null) return;
+        System.out.println(prefijo + nodo.getDato());
+
+
+        if (nodo.getSubArbol() != null) {
+            imprimir(nodo.getSubArbol(), prefijo + "  ");
+        }
+        imprimir(nodo.getLiga(), prefijo);
+    }
 }
--- src/logic/menu.java
+++ src/logic/menu.java
@@ -1,6 +1,7 @@
 package logic;
 import java.util.Scanner;
 
+
 public class menu {
     public static void main(String[] args) {
         arbolGeneralizado arbol = new arbolGeneralizado();
@@ -8,33 +9,49 @@ public class menu {
         boolean bandera = false;
         int opcion;
 
+
         do {
             System.out.println("""
-            -----------------------------------------------------------------------
-                    Ingrese el número de alguna de las siguientes opciones:
+           -----------------------------------------------------------------------
+                   Ingrese el número de alguna de las siguientes opciones:
 
-                1. Crear Árbol
-                2. Buscar Nodo
-                0. Salir
-            -----------------------------------------------------------------------
-            """);
+
+               1.  Crear Árbol             (Listo)
+               2.  Buscar Nodo             (Listo)
+               3.  Insertar dato           (Listo)
+               4.  Eliminar dato           (En Proceso)
+               5.  Mostrar raíces          (Listo)
+               6.  Mostrar hojas           (Listo)
+               7.  Altura de un dato       (Falta, En proceso)
+               8.  Nivel de un dato        (Listo, Falta Validar si no existe)
+               9.  Imprimir Datos          (Listo)
+               10. Mostrar Arbol           (Falta)
+               11. Reto 1                  (Falta)
+               12. Reto 2                  (No creo que se haga)
+               0. Salir
+           -----------------------------------------------------------------------
+           """);
+
 
             opcion = scanner.nextInt();
+
 
             switch (opcion) {
                 case 1:
                     System.out.println("""
-                            \nPor favor ingrese el árbol con el siguiente formato:
-                            Padres: {
-                            Hermanos: ,
-                            Ejemplo: A{B C{E{G H} F} D}
-                            """);
+                           \nPor favor ingrese el árbol con el siguiente formato:
+                           Padres: {
+                           Hermanos: ,
+                           Ejemplo: A{B C{E{G H} F} D}
+                           """);
+
 
                     scanner.nextLine();
                     String arbolEnString = scanner.nextLine();
                     arbol.insertarDatos(arbolEnString);
                     System.out.println("Árbol construido correctamente.");
                     break;
+
 
                 case 2:
                     System.out.print("Ingrese el carácter a buscar: ");
@@ -47,12 +64,47 @@ public class menu {
                     }
                     break;
                 case 3:
-
+                    System.out.print("Ingrese el Valor del nodo padre: ");
+                    char Padre = scanner.next().charAt(0);
+                    System.out.print("Ingrese el valord del nodo hijo: ");
+                    char Hijo = scanner.next().charAt(0);
+                    Nodo Insertar = arbol.insertarLetra(Hijo, Padre);
+                    if (Insertar != null) {
+                        System.out.println("!!!Nodo " + Hijo + " se agrego correctamente!!!");
+                    } else {
+                        System.out.println("El Nodo padre ingresado no existe.");
+                    }
+                    break;
+                case 5:
+                    int C = 1;
+                    arbol.mostrarRaices(arbol.getRaiz(), C);
+                    break;
+                case 6:
+                    arbol.mostrarHijos(arbol.getRaiz());
+                    break;
+                case 7:
+                    System.out.println("Ingrese la letra a buscar su altura: ");
+                    char letra = scanner.next().charAt(0);
+                    int altura = arbol.encontrarAltura(arbol.getRaiz(), letra);
+                    if (altura != -1) {
+                        System.out.println("La altura del nodo " + letra + " es: " + altura);
+                    }
+                    break;
+                case 8:
+                    int Nivel = 0;
+                    System.out.println("Ingrese el valor del nodo a evular su nivel: ");
+                    char Dato = scanner.next().charAt(0);
+                    arbol.nivel(arbol.getRaiz(), Dato, Nivel);
+                    break;
+                case 9:
+                    System.out.println("Su Arbol es:");
+                    arbol.imprimir(arbol.getRaiz(), "");
                     break;
                 case 0:
                     System.out.println("Saliendo del menú...");
                     bandera = true;
                     break;
+
 
                 default:
                     System.out.println("!!! OPCIÓN NO VÁLIDA !!!");
@@ -60,7 +112,7 @@ public class menu {
             }
         } while (!bandera);
 
+
         scanner.close();
     }
 }
-
